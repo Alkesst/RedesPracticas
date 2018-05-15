b68120e2c567ae69364f94159344c6370e244f81
Alkesst <alec.augal@gmail.com>
2018-05-15 14:13:49 +0200

    Server nos handles timeouts

--- src/Sockets/Server.java
+++ src/Sockets/Server.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.sql.SQLOutput;
 
 public class Server{
@@ -14,28 +15,34 @@ public class Server{
             ServerSocket serverSocket = new ServerSocket(12345);
             System.out.println("Port opened");
             for(;;){
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                pw.println("Binvenido al servicio de cifrado");
-                String inetClientAddress = clientSocket.getInetAddress().toString();
-                System.out.println("Client " + inetClientAddress + " connected...");
-                int cesarOffset;
-                String receivedMessage = "";
-                String messageToSend = "";
-                do{
-                    cesarOffset = Integer.parseInt(br.readLine());
-                    if (cesarOffset != 0) {
-                        receivedMessage = br.readLine();
-                        messageToSend = codifyString(receivedMessage, cesarOffset);
-                        pw.println(messageToSend);
-                        System.out.println("Client " + inetClientAddress + " sends offset: " + cesarOffset);
-                        System.out.println("Client " + inetClientAddress + " sends message: " + receivedMessage);
-                        System.out.println("Message sent to client: " + messageToSend);
-                    }
-                }while(cesarOffset != 0);
-                pw.println("OK");
-                clientSocket.close();
+                    Socket clientSocket = serverSocket.accept();
+                    PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    pw.println("Binvenido al servicio de cifrado");
+                    String inetClientAddress = clientSocket.getInetAddress().toString();
+                    System.out.println("Client " + inetClientAddress + " connected...");
+                    int cesarOffset;
+                    String receivedMessage = "";
+                    String messageToSend = "";
+                    try {
+                    clientSocket.setSoTimeout(40000);
+                    do {
+                        cesarOffset = Integer.parseInt(br.readLine());
+                        if (cesarOffset != 0) {
+                            receivedMessage = br.readLine();
+                            messageToSend = codifyString(receivedMessage, cesarOffset);
+                            pw.println(messageToSend);
+                            System.out.println("Client " + inetClientAddress + " sends offset: " + cesarOffset);
+                            System.out.println("Client " + inetClientAddress + " sends message: " + receivedMessage);
+                            System.out.println("Message sent to client: " + messageToSend);
+                        }
+                    } while (cesarOffset != 0);
+                    pw.println("OK");
+                    clientSocket.close();
+                } catch (SocketTimeoutException e){
+                    System.err.println("Connection timeout with " + clientSocket.getInetAddress());
+                    clientSocket.close();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
