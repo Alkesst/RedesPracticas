3cffd0b75187ff645efd586cbcfd6e11639bf02a
JesusPa98 <jesusparejo98@gmail.com>
2018-06-01 20:39:25 +0200

    Practica 4 comentada

--- Prácica 4/src/Sockets/Client.java
+++ Prácica 4/src/Sockets/Client.java
@@ -7,35 +7,35 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         try {
-            Scanner consoleInput = new Scanner(System.in);
+            Scanner consoleInput = new Scanner(System.in);                                                              // Creamos un escaner para leer la IP y el puerto
             String currentInput;
             String cesarOffset;
             System.out.println("Enter the host IP: ");
-            String host = consoleInput.nextLine();
+            String host = consoleInput.nextLine();                                                                      // Leemos la IP
             System.out.println("Enter the port: ");
-            int port = consoleInput.nextInt();
-            Socket sock = new Socket(host, port);
-            System.out.println("Connected to " + host + " at port " + port);
-            PrintWriter pw = new PrintWriter(sock.getOutputStream(), true);
-            BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            System.out.println(br.readLine());
+            int port = consoleInput.nextInt();                                                                          // Leemos el puerto
+            Socket sock = new Socket(host, port);                                                                       // Creamos el cliente
+            System.out.println("Connected to " + host + " at port " + port);                                            // Imprimimos por pantalla la conexion y el puerto
+            PrintWriter pw = new PrintWriter(sock.getOutputStream(), true);                                   // Creamos el printWriter para enviar
+            BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));                       // Creamos el bufferReader para leer (recibir)
+            System.out.println(br.readLine());                                                                          // Leemos la primera lia que recibimos (bienvenida)
             consoleInput.nextLine();
             do {
                 System.out.println("Enter the offset of the codification!: ");
-                cesarOffset = consoleInput.nextLine();
-                if (!cesarOffset.equals("0")) {
-                    System.out.println("Enter the string to codificar!: ");
-                    currentInput = consoleInput.nextLine();
-                    pw.print(cesarOffset + "\r\n");
-                    pw.println(currentInput);
-                    System.out.println("Esperando respuesta del servidor...");
-                    System.out.println(br.readLine());
+                cesarOffset = consoleInput.nextLine();                                                                  // Introduce el offset
+                if (!cesarOffset.equals("0")) {                                                                         // Si el offset es distinto de 0:
+                    System.out.println("Enter the string to codify!: ");
+                    currentInput = consoleInput.nextLine();                                                             // Leemos el mensaje a codificar
+                    pw.print(cesarOffset + "\r\n");                                                                     // Le enviamos el offser al servidor
+                    pw.println(currentInput);                                                                           // Le enviamos el mensaje al servidor
+                    System.out.println("Waiting server response...");
+                    System.out.println(br.readLine());                                                                  // Recibimos la respuesta del servidor
                 }
-            } while (!cesarOffset.equals("0"));
-            pw.println("0");
-            System.out.println("Esperando respuesta del servidor...");
-            System.out.println(br.readLine());
-            sock.close();
+            } while (!cesarOffset.equals("0"));                                                                         // Seguir el bucle mientras el offset sea distinto de 0
+            pw.println("0");                                                                                            // Enviar un 0 para terminar la conexion
+            System.out.println("Waiting server response...");
+            System.out.println(br.readLine());                                                                          // Recibir respuesta para cerrar la conexion (OK)
+            sock.close();                                                                                               // Conexion cerrada
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Prácica 4/src/Sockets/Server.java
+++ Prácica 4/src/Sockets/Server.java
@@ -12,58 +12,62 @@ import java.sql.SQLOutput;
 public class Server{
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(12345);
+            ServerSocket serverSocket = new ServerSocket(12345);                                                  // Creamos el Servior con el puerto 12345
             System.out.println("Port opened");
-            for(;;){
-                    Socket clientSocket = serverSocket.accept();
-                    PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
-                    BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    pw.println("Binvenido al servicio de cifrado");
-                    String inetClientAddress = clientSocket.getInetAddress().toString();
-                    System.out.println("Client " + inetClientAddress + " connected...");
+            for(;;){//Bucle infinito
+                    Socket clientSocket = serverSocket.accept();                                                        // Aceptar la conexion
+                    PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);                   // Creamos el PrintWriter para poder enviar
+                    BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));       // Creamos el BufferedReader para leer (recibir)
+                    pw.println("Binvenido al servicio de cifrado");                                                     // Enviamos al cliente el mensaje de bienvenida
+                    String inetClientAddress = clientSocket.getInetAddress().toString();                                // Guardamos la IP del equipo que se ha conectado
+                    System.out.println("Client " + inetClientAddress + " connected...");                                // Imprimimos la IP del cliente conectado
                     int cesarOffset;
                     String receivedMessage = "";
                     String messageToSend = "";
                     try {
-                    clientSocket.setSoTimeout(40000);
+                    clientSocket.setSoTimeout(40000);                                                                   // En caso de que no enviar nada, cerrar la conexion con el Timeout
                     do {
-                        cesarOffset = Integer.parseInt(br.readLine());
-                        if (cesarOffset != 0) {
-                            receivedMessage = br.readLine();
-                            messageToSend = codifyString(receivedMessage, cesarOffset);
-                            pw.println(messageToSend);
-                            System.out.println("Client " + inetClientAddress + " sends offset: " + cesarOffset);
-                            System.out.println("Client " + inetClientAddress + " sends message: " + receivedMessage);
-                            System.out.println("Message sent to client: " + messageToSend);
+                        cesarOffset = Integer.parseInt(br.readLine());                                                  // Obtenemos la cantidad para hacer el offset
+                        if (cesarOffset != 0) {                                                                         // Si es distinto de 0:
+                            receivedMessage = br.readLine();                                                            // Leemos el mensaje enviado
+                            messageToSend = codifyString(receivedMessage, cesarOffset);                                 // Codificamos el mensaje a enviar
+                            pw.println(messageToSend);                                                                  // Enviamos el mensaje codificado
+                            System.out.println("Client " + inetClientAddress + " sends offset: " + cesarOffset);        // Imprimimos el offset
+                            System.out.println("Client " + inetClientAddress + " sends message: " + receivedMessage);   // Imprimimos el mensaje sin codificar
+                            System.out.println("Message sent to client: " + messageToSend);                             // Imprimimos el mensaje codificado
                         }
-                    } while (cesarOffset != 0);
-                    pw.println("OK");
-                    clientSocket.close();
-                } catch (SocketTimeoutException e){
-                    System.err.println("Connection timeout with " + clientSocket.getInetAddress());
-                    clientSocket.close();
+                    } while (cesarOffset != 0);                                                                         // Bucle mientras el offset sea distinto de 0
+                    pw.println("OK");                                                                                   // Le enviamos un OK al cliente para cerrar la conexion
+                    clientSocket.close();                                                                               // Cerramos la conexion
+                } catch (SocketTimeoutException e){                                                                     // En caso de que haya saltado el timeout
+                    System.err.println("Connection timeout with " + clientSocket.getInetAddress());                     // Imprimimos por pantalla un mensaje de error
+                    clientSocket.close();                                                                               // Cerramos la conexion
                 }
-            }
+            }// fin for
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String codifyString(String receivedMessage, int offset){
+    /**
+     * @param receivedMessage
+     * @param offset
+     */
+    private static String codifyString(String receivedMessage, int offset){                                             // Funcion para codificar el mensaje
         StringBuilder codified =  new StringBuilder();
         char currentChar;
         int numLetras = 'Z' - 'A' + 1;
-        offset %= numLetras;
+        offset %= numLetras;                                                                                            // Si el offset se pasa de 26, lo recalculamos con el módulo
         for(int i = 0; i < receivedMessage.length(); i++){
-            currentChar = receivedMessage.charAt(i);
-            if(Character.isLetter(currentChar)){
-                if((Character.isLowerCase(currentChar) && currentChar + offset > 'z')
+            currentChar = receivedMessage.charAt(i);                                                                    // Cogemos el char para hacer la codificación
+            if(Character.isLetter(currentChar)){                                                                        // Si es una letra:
+                if((Character.isLowerCase(currentChar) && currentChar + offset > 'z')                                   // Si la letra está antes de la z o Z
                         || (Character.isUpperCase(currentChar) && currentChar + offset > 'Z')){
-                    currentChar -= numLetras;
+                    currentChar -= numLetras;                                                                           // Recalculamos el char actual
                 }
-                currentChar += offset;
+                currentChar += offset;                                                                                  // Aplicamos la codificación
             }
-            codified.append(currentChar);
+            codified.append(currentChar);                                                                               // Añadimos el char a la cadena
         }
         return codified.toString();
     }
